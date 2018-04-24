e82a8f3b7a1d324b4e1301053207728bc8c3574e
Kevin Amiranoff <kamiranoff@gmail.com>
2018-04-25 00:16:18 +0100

    Adds react-navigation and pin versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import configureStore from './store';
-import colors from './constants/colors';
-import DIMENSIONS from './constants/dimensions';
-import Auth from './Auth';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: DIMENSIONS.WINDOW_HEIGHT * 0.1,
-    alignItems: 'center',
-    backgroundColor: colors.beach5,
-  },
-});
+import Entry from './Entry';
 
 // GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest;
 
@@ -22,9 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-          <Auth />
-        </View>
+          <Entry />
       </Provider>
     );
   }
